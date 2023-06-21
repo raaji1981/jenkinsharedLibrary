49ba549b0581af355ce8177cb959aa0702b67017
raaji1981 <76651583+raaji1981@users.noreply.github.com>
2023-06-21 19:44:11 +0530

    Create fetchrepo.groovy

---
+++ vars/fetchrepo.groovy
@@ -0,0 +1,14 @@
+def callrepo(String repoUrl){
+  pipeline{
+    agent any
+    stages{
+      stage("init"){
+        steps{
+          git_branch: 'main'
+          url: "${repoUrl}
+        }
+      }
+    }
+  }
+          
+  
